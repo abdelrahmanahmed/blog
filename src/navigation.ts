819c23d0835c3c3a7076acc92c231c4924c91493
abdelrahmanahmed <abdelrahman.wahdan@outlook.com>
2024-09-22 14:20:48 +0200

    init new blog

---
+++ src/navigation.ts
@@ -0,0 +1,29 @@
+import { getPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Homes',
+      links: [
+        {
+          text: 'Personal',
+          href: getPermalink('/'),
+        },
+      ],
+    },
+    {
+      text: 'Blog (coming soon)',
+    },
+  ],
+  actions: [{ text: 'Hire Me', href: 'https://www.linkedin.com/in/wahdan', target: '_blank' }],
+};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/wahdan/' },
+    { ariaLabel: 'stackoverflow', icon: 'tabler:brand-stackoverflow', href: 'https://stackoverflow.com/users/1846087/wahdan' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/abdelrahmanahmed' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  footNote: `© 2024 Abdelrahman Wahdan. All rights reserved.`,
+};
